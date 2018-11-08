d54920e202a8e30e37604d076a41e00c6b13e9b2
arun subramanian <ArunSubramanian@aruns-MBP.frontierlocal.net>
2018-11-08 01:07:20 -0500

    Created a random unsub token for each user

--- app/controllers/subscription_settings_controller.rb
+++ app/controllers/subscription_settings_controller.rb
@@ -1,6 +1,6 @@
 class SubscriptionSettingsController < ApplicationController
   def index
-    @user = User.find_by(email: params[:email], unsub_token: params[:token])
+    @user = User.select([:id, :chat_notification_subscription, :newsletter_subscription]).find_by(email: params[:email], unsub_token: params[:token])
     raise ActionController::RoutingError.new('Not Found') if @user.blank?
   end
 end

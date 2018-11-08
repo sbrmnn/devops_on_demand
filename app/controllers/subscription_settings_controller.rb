a23ba38d7add9e88ce1fa9d2e8ce7bb828bdd7bf
arun subramanian <ArunSubramanian@aruns-MBP.frontierlocal.net>
2018-11-08 00:55:37 -0500

    Added routing error

--- app/controllers/subscription_settings_controller.rb
+++ app/controllers/subscription_settings_controller.rb
@@ -1,5 +1,6 @@
 class SubscriptionSettingsController < ApplicationController
   def index
     @user = User.find_by(email: params[:email], unsub_token: params[:token])
+    raise ActionController::RoutingError.new('Not Found') if @user.blank?
   end
 end

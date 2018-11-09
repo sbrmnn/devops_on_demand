ca12f145391ce7dbff8948745830bd91965dddc6
arun subramanian <ArunSubramanian@aruns-MBP.frontierlocal.net>
2018-11-09 15:04:16 -0500

    Added healthcheck

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
 
 
+  resources :healthcheck, only: [:index]
+
   resource :subscription_settings, only: [:show, :update]
 
   resources :chatrooms, module: "chatrooms" do
---
+++ test/controllers/healthcheck_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class HealthcheckControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get healthcheck_index_url
+    assert_response :success
+  end
+
+end

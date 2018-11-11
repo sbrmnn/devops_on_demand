176a97a94af64e3d8c37b37b55e47bccf5823079
arun subramanian <ArunSubramanian@aruns-MBP.frontierlocal.net>
2018-11-11 10:05:55 -0500

    Escaping messages before saving

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,13 @@
 class Message < ApplicationRecord
-  belongs_to :chatroom
-  belongs_to :user
+  before_save :escape_body
+  belongs_to  :chatroom
+  belongs_to  :user
   validates_presence_of :user
+
+
+  protected
+
+  def escape_body
+    self.body = ERB::Util.html_escape(body)
+  end
 end

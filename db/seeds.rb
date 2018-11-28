aac83cd6e8d4d70c02e7dd74c58d3761a23e3cb7
arun subramanian <ArunSubramanian@aruns-MBP.frontierlocal.net>
2018-11-27 19:04:10 -0500

    Changed constants

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-AWS = 'AWS'
-AZURE = 'MS Azure'
-GOOGLE = 'Google'
+AWS_CERT = 'AWS'
+AZURE_CERT = 'MS Azure'
+GOOGLE_CERT = 'Google'
 
-CertificationName.first_or_create(provider: AWS, name: 'Certified Cloud Practitioner')
-CertificationName.first_or_create(provider: AWS, name: 'Certified Developer – Associate')
-CertificationName.first_or_create(provider: AWS, name: 'Certified SysOps Administrator – Associate')
-CertificationName.first_or_create(provider: AWS, name: 'Certified Solutions Architect – Associate')
-CertificationName.first_or_create(provider: AWS, name: 'Certified DevOps Engineer – Professional')
-CertificationName.first_or_create(provider: AWS, name: 'Certified Solutions Architect – Professional')
-CertificationName.first_or_create(provider: AWS, name: 'Certified Big Data – Specialty')
-CertificationName.first_or_create(provider: AWS, name: 'Certified Advanced Networking – Specialty')
+CertificationName.first_or_create(provider: AWS_CERT, name: 'Certified Cloud Practitioner')
+CertificationName.first_or_create(provider: AWS_CERT, name: 'Certified Developer – Associate')
+CertificationName.first_or_create(provider: AWS_CERT, name: 'Certified SysOps Administrator – Associate')
+CertificationName.first_or_create(provider: AWS_CERT, name: 'Certified Solutions Architect – Associate')
+CertificationName.first_or_create(provider: AWS_CERT, name: 'Certified DevOps Engineer – Professional')
+CertificationName.first_or_create(provider: AWS_CERT, name: 'Certified Solutions Architect – Professional')
+CertificationName.first_or_create(provider: AWS_CERT, name: 'Certified Big Data – Specialty')
+CertificationName.first_or_create(provider: AWS_CERT, name: 'Certified Advanced Networking – Specialty')
+CertificationName.first_or_create(provider: AWS_CERT, name: 'Certified Security – Specialty')
+
+CertificationName.first_or_create(provider: AZURE_CERT, name: 'Certified Solutions Associate (MCSA)')
+CertificationName.first_or_create(provider: AZURE_CERT, name: 'Certified Solutions Expert (MCSE)')
+CertificationName.first_or_create(provider: AZURE_CERT, name: 'Certified Solutions Developer (MCSD)')
+
+
+CertificationName.first_or_create(provider: GOOGLE_CERT, name: 'Associate Cloud Engineer')
+CertificationName.first_or_create(provider: GOOGLE_CERT, name: 'Professional Cloud Architect')
+CertificationName.first_or_create(provider: GOOGLE_CERT, name: 'Professional Data Engineer')
+CertificationName.first_or_create(provider: GOOGLE_CERT, name: 'Professional Cloud Developer')

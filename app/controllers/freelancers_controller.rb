8cb11ed5c55613d5e6be1e32124341d8ade367b9
arun subramanian <ArunSubramanian@aruns-MBP.frontierlocal.net>
2018-12-23 07:22:42 -0500

    Redirect on sign in

--- app/controllers/freelancers_controller.rb
+++ app/controllers/freelancers_controller.rb
@@ -1,8 +1,10 @@
 class FreelancersController < ApplicationController
   layout 'profile', only: :show
-  before_action :authenticate_user!
-  
+
   def index
+    if user_signed_in?
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def show

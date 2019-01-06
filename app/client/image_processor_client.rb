56925d7007e868930c154e61a859e934fe8f0aa7
arun subramanian <ArunSubramanian@aruns-MBP.frontierlocal.net>
2019-01-06 14:42:38 -0500

    Refactored bug

--- app/client/image_processor_client.rb
+++ app/client/image_processor_client.rb
@@ -3,7 +3,7 @@ class ImageProcessorClient
   def self.get_profile_image_url(image_path)
     return nil if image_path.blank?
     public_id = get_public_id(image_path)
-    return nil if public_id.present?
+    return nil if public_id.blank?
     dim = ProfileImageDimensions.instance
     Cloudinary::Utils.cloudinary_url(public_id , {height: dim.height, width: dim.width, crop: dim.crop, gravity: dim.gravity})
   end

ca9594a9d1616abc18d3d6f132fd8a383216b246
arun subramanian <ArunSubramanian@aruns-MBP.frontierlocal.net>
2019-01-06 14:16:58 -0500

    Refactored method

--- app/client/image_processor_client.rb
+++ app/client/image_processor_client.rb
@@ -1,15 +1,13 @@
 class ImageProcessorClient
+
   def self.get_profile_image_url(image_path)
-    if image_path
-      public_id = get_public_id(image_path)
-      if public_id.present?
-        dim = ProfileImageDimensions.instance
-        Cloudinary::Utils.cloudinary_url(public_id , {height: dim.height, width: dim.width, crop: dim.crop, gravity: dim.gravity})
-      else
-        nil
-      end
-    end
+    return nil if image_path.blank?
+    public_id = get_public_id(image_path)
+    return nil if public_id.present?
+    dim = ProfileImageDimensions.instance
+    Cloudinary::Utils.cloudinary_url(public_id , {height: dim.height, width: dim.width, crop: dim.crop, gravity: dim.gravity})
   end
+
   def self.get_public_id(image_path)
     return nil if image_path.blank?
     preloaded = Cloudinary::PreloadedFile.new(image_path)

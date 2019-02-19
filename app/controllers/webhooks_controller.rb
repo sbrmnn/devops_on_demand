851730082487712c67c6ca848f9c48b47fef28d3
arun subramanian <ArunSubramanian@aruns-MBP.frontierlocal.net>
2019-02-18 19:01:41 -0500

    Checking for stripe signature

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,8 +1,19 @@
 class WebhooksController < ApplicationController
   protect_from_forgery :except => :create
-  
+
   def create
+    payload = request.body.read
+    sig_header = request.env['HTTP_STRIPE_SIGNATURE']
+    begin
+     Stripe::Webhook.construct_event(
+          payload, sig_header, endpoint_secret
+      )
+    rescue JSON::ParserError => e
+      render status: 400 and return
+    rescue Stripe::SignatureVerificationError => e
+      render status: 400 and return
+    end
     event_json = JSON.parse(request.body.read)
-    render json: event_json
+    render json: event_json, status: 200
   end
 end

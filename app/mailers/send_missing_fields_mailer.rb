6b48e7d87f40f9bed9609814d39b25ef814d84fe
arun subramanian <ArunSubramanian@aruns-MBP.frontierlocal.net>
2019-05-05 18:26:38 -0400

    Revised email

--- app/mailers/send_missing_fields_mailer.rb
+++ app/mailers/send_missing_fields_mailer.rb
@@ -4,6 +4,7 @@ class SendMissingFieldsMailer < ApplicationMailer
   def send_to_freelancer(freelancer)
     @user = freelancer.user
     @freelancer_name = @user.first_name
+    @host = ENV['HOST_URL']
     mail(to: @user.email, subject: "We need more information from you.")
   end
 end

c04978db44fa6637fac3572f3843b34c7a8d0e6d
arun subramanian <ArunSubramanian@aruns-MBP.frontierlocal.net>
2019-05-06 13:00:44 -0400

    Added get list of freelancers

---
+++ app/queries/get_list_of_skills_from_freelancers.rb
@@ -0,0 +1,5 @@
+class GetListOfSkillsFromFreelancers
+  def self.call
+     CloudService.joins(:skills).group("cloud_services.id").having('count(skills) > 0').pluck(:id, :name, :provider).group_by { |c| c[2] }
+  end
+end

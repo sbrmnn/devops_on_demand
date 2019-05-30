d663cf33a0f92a70cdebd381608b4c7f078624c8
arun subramanian <ArunSubramanian@aruns-MBP.frontierlocal.net>
2019-05-30 07:56:25 -0400

    Recalculated platform rate

--- app/services/calculate_platform_rate.rb
+++ app/services/calculate_platform_rate.rb
@@ -13,7 +13,16 @@ class CalculatePlatformRate
 
   private
 
-  def platform_rate_algo(rate)
-    (rate * 1.20).ceil
+   def platform_rate_algo(rate)
+     commission = 0
+    case rate
+      when (100..Float::INFINITY)
+        commission = 7.5 + 4 + ((rate-50) * 0.15)
+      when (50...100)
+        commission = 7.5 + ((rate - 30) * 0.20)
+      else
+        commission =  rate * 0.25
+    end
+     (rate + commission).ceil
+   end
   end
-end
